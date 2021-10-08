7b4208fdb013f1f44c28aa0aeaa933f470625b80
Ajeej16 <f07101116@gmail.com>
2021-10-07 21:01:04 -0400

    Taking out the Home component from the About page

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,17 +1,12 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <home/>
   </div>
 </template>
 
 <script>
-import Home from '@/views/Home'
 export default {
   name: "About",
 
-  components: {
-    Home,
-  }
 }
 </script>
