2dddb4c5275b706c99260728c3f49dde05525c98
Ajeej16 <f07101116@gmail.com>
2021-10-09 16:52:55 -0400

    Added extra inputs for registration and validation for registration inputs
    Added a username and confirmation password in the registration page. Also added a way to show validation messages when input is not made properly. Currently not working completely since I am only doing the validation on the server side. Planning to change up the client and server files in order to have a easier time to share documents between them. This will allow for them to both do validation with a common validation middleware.

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -7,17 +7,39 @@
           cols="12"
           md="4"
         >
-          <v-text-field type="email" name="email" v-model="email" placeholder="email" />
+          <v-text-field type="username" name="username" v-model="username" placeholder="username" 
+            :error-messages="errors.username"
+          />
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field type="email" name="email" v-model="email" placeholder="email" 
+            :error-messages="errors.email"
+          />
         </v-col>
 
         <v-col
         cols="12"
         md="4"
         >
-          <v-text-field type="password" name="password" v-model="password" placeholder="password" />
+          <v-text-field type="password" name="password" v-model="password" placeholder="password" 
+            :error-messages="errors.password"
+          />
         </v-col>
-      <div class="error" v-html="error" />
-      <v-btn color="success" @click="register">Register</v-btn>
+
+        <v-col
+        cols="12"
+        md="4"
+        >
+          <v-text-field type="confirm_password" name="confirm_password" v-model="confirm_password" placeholder="confirm_password" 
+            :error-messages="errors.confirm_password"
+          />
+          <div class="error" v-html="errors.confirm_password" />
+        </v-col>
+      <v-btn color="success" v-on:click.prevent="register">Register</v-btn>
       </v-row>
     </v-container>
   </v-form>
@@ -30,9 +52,11 @@ export default {
 
   data () {
     return {
+      username: '',
       email: '',
       password: '',
-      error: null
+      confirm_password: '',
+      errors: {}
     }
   },
 
@@ -40,12 +64,17 @@ export default {
     async register () {
       try {
         await AuthenticationService.register({
+          username: this.username,
           email: this.email,
-          password: this.password
+          password: this.password,
+          confirm_password: this.confirm_password
         })
-        this.error = null
+        this.errors = null
       } catch (error) {
-        this.error = error.response.data.error
+        const errors = error.response.data.errors
+        for(let i = 0; i < errors.length; i++) {
+          this.errors[errors[i].label] = errors[i].message
+        }
       }
     }
   }
@@ -54,6 +83,6 @@ export default {
 
 <style scoped>
 .error {
-  color: red;
+  background-color: red;
 }
 </style>
--- server/src/policies/AuthenticationControllerPolicy.js
+++ server/src/policies/AuthenticationControllerPolicy.js
@@ -3,24 +3,37 @@ const Joi = require('joi')
 module.exports = {
   register (req, res, next) {
     const schema = Joi.object({
-      email: Joi.string().email().messages({
-        'string.email': '"{#value}" is not a valid email.'
+      username: Joi.string().min(6).max(32).required().messages({
+        'any.required': '{#label} is required.',
+        'string.empty': '{#label} is required.',
+        'string.min': '{#label} must be at least {#limit} characters.',
+        'string.max': '{#label} must be no greater than {#limit} characters.'
       }),
-      password: Joi.string().min(8).max(32).regex(
-        new RegExp('^[a-zA-Z0-9]$') //eslint-disable-line
+      email: Joi.string().email().required().messages({
+        'any.required': '{#label} is required.',
+        'string.empty': '{#label} is required.',
+        'string.email': '{#value} is not a valid email.'
+      }),
+      password: Joi.string().min(6).max(32).required().regex(
+        new RegExp('^[a-zA-Z0-9]{6,32}$') //eslint-disable-line
       ).messages({
-        'string.min': '"password" must be at least 8 characters.',
-        'string.max': '"password" must be no greater than 32 characters.',
-        'string.pattern.base': '"password" with value "{#value}" failed to match the required pattern: a-z, A-Z, 0-9'
+        'any.required': '{#label} is required.',
+        'string.empty': '{#label} is required.',
+        'string.min': '{#label} must be at least {#limit} characters.',
+        'string.max': '{#label} must be no greater than {#limit} characters.',
+        'string.pattern.base': '{#label} with value "{#value}" failed to match the required pattern: a-z, A-Z, 0-9.'
+      }),
+      confirm_password: Joi.string().valid(Joi.ref('password')).required().messages({
+        'any.required': '{#label} is required.',
+        'string.empty': '{#label} is required.',
+        'any.only': '{#label} does not match "password"'
       })
-    })
+    }).options({ abortEarly: false })
 
-    const { error, value } = schema.validate(req.body) //eslint-disable-line
+    const { error } = schema.validate(req.body) //eslint-disable-line
 
     if (error) {
-      res.status(400).send({
-        error: error.details
-      })
+      res.status(400).send({ errors: (error.details || []).map(er => { return { label: er.path[0], message: er.message } }) })
     } else {
       next()
     }
